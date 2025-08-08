2d85d93e718ef7e3172af4eb57e9973056bb5c91
thecodebeneath <>
2025-08-08 12:42:28 -0500

    Remove gitlab vars since moving to OIDC and ECR cred helper don't need them

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,10 +9,6 @@ terraform {
 }
 
 provider "aws" {
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-  token      = var.AWS_SESSION_TOKEN
-  region     = var.AWS_REGION
   default_tags {
     tags = {
       Environment = "codebeneath-lab"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,27 +1,3 @@
-variable AWS_ACCESS_KEY_ID {
-  type        = string
-  nullable    = false
-  description = "AWS session credentials"
-}
-
-variable AWS_SECRET_ACCESS_KEY {
-  type        = string
-  nullable    = false
-  description = "AWS session credentials"
-}
-
-variable AWS_SESSION_TOKEN {
-  type        = string
-  nullable    = false
-  description = "AWS session credentials"
-}
-
-variable AWS_REGION {
-  type        = string
-  default     = "us-east-2"
-  description = "AWS session credentials"
-}
-
 variable project_name {
   type        = string
   default     = "codebeneath"
